2df458fff3618e3c43942f514b243e06b62c1339
fmwang <fmwang@unity3d.com>
2020-11-23 09:36:38 +0800

    Add Rendering Context

--- Hazel/src/Platform/Windows/WindowsWindow.h
+++ Hazel/src/Platform/Windows/WindowsWindow.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Hazel/Window.h"
+#include "Hazel/Renderer/GraphicsContext.h"
 
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
@@ -31,6 +32,7 @@ namespace Hazel {
 		virtual void Shutdown();
 	private:
 		GLFWwindow* m_Window;
+		GraphicsContext* m_Context;
 
 		struct WindowData
 		{
