8b37749b888fbcad73de3ffc718ca0fa1e1c9f56
fmwang <fmwang@unity3d.com>
2020-11-24 09:22:45 +0800

    Add First Triangle

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -35,6 +35,7 @@ namespace Hazel {
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
+		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
 	private:
 		static Application* s_Instance;
 	};
@@ -43,5 +44,3 @@ namespace Hazel {
 	// Should be defined in CLIENT
 	Application* CreateApplication();
 }
-
-
